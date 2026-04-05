7a840307151ba7b3843789c0c3c306589a3e9305
jayden <46307433+kermeow@users.noreply.github.com>
2026-04-06 00:31:37 +0100

    gVoiceStates

---
+++ mod/audio-bridge.lua
@@ -0,0 +1,7 @@
+gVoiceStates = {}
+for i = 0, MAX_PLAYERS - 1 do
+    gVoiceStates[i] = {
+        volume = 1,
+        speakVol = 1,
+    }
+end
--- mod/djui-settings.lua
+++ mod/djui-settings.lua
@@ -1,12 +1,3 @@
--- Should move to a util or init file
-gVoiceStates = {}
-for i = 0, MAX_PLAYERS - 1 do
-    gVoiceStates[i] = {
-        volume = 1,
-        speakVol = 1,
-    }
-end
-
 -- Util render funcs --
 
 ---@param x integer
