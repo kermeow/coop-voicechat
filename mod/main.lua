153989f326b610f24b475dd8772e2a0e6af09d9d
jayden <46307433+kermeow@users.noreply.github.com>
2026-04-07 02:09:46 +0100

    make mod description fit

--- mod/main.lua
+++ mod/main.lua
@@ -1,4 +1,4 @@
 -- name: Voice Chat
--- description: Voice Chat\n\nA collaboration between \\#008800\\Squishy6094 \\#dcdcdc\\& \\#abcdef\\kermeow
+-- description: Voice Chat\nFrom \\#008800\\Squishy6094 \\#dcdcdc\\& \\#abcdef\\kermeow
 
 -- name subject to change
