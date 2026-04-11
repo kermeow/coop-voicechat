fa49a206aef9002d9bc16bb98d0ea0c97aa6092a
jayden <46307433+kermeow@users.noreply.github.com>
2026-04-11 10:52:50 +0100

    add player streamer to audio bridge

--- client/audio/audio_bridge.go
+++ client/audio/audio_bridge.go
@@ -2,15 +2,41 @@ package audio
 
 import (
 	"context"
+	"coop-voicechat/coop"
 	"log"
+
+	"github.com/gopxl/beep/v2/speaker"
 )
 
 type AudioBridge struct {
+	streamers map[uint8]*PlayerStreamer
 }
 
 func NewAudioBridge() *AudioBridge {
-	a := &AudioBridge{}
+	a := &AudioBridge{
+		streamers: make(map[uint8]*PlayerStreamer),
+	}
 	return a
+}
+
+func (b *AudioBridge) AddPlayer(player *coop.Player) {
+	b.RemovePlayer(player.LocalIndex)
+
+	log.Println("Adding speaker", player.LocalIndex)
+
+	s := NewPlayerStreamer(player)
+	b.streamers[player.LocalIndex] = s
+	
+	speaker.Play(s)
+}
+
+func (b *AudioBridge) RemovePlayer(localIndex uint8) {
+	s := b.streamers[localIndex]
+	if s != nil {
+		log.Println("Removing speaker", localIndex)
+
+		delete(b.streamers, localIndex)
+	}
 }
 
 func (b *AudioBridge) Run(ctx context.Context) {
---
+++ client/audio/player_streamer.go
@@ -0,0 +1,40 @@
+package audio
+
+import (
+	"coop-voicechat/coop"
+
+	"github.com/gopxl/beep/v2/effects"
+)
+
+type PlayerStreamer struct {
+	player   *coop.Player
+	streamer *OpusStreamer
+
+	volume *effects.Volume
+}
+
+func NewPlayerStreamer(player *coop.Player) *PlayerStreamer {
+	s := &PlayerStreamer{
+		player:   player,
+		streamer: NewOpusStreamer(),
+	}
+	s.volume = &effects.Volume{
+		Streamer: s.streamer,
+		Base:     2,
+		Volume:   0,
+		Silent:   false,
+	}
+	return s
+}
+
+func (s *PlayerStreamer) Stream(samples [][2]float64) (n int, ok bool) {
+	if s.player.LocalIndex < 1 {
+		return 0, true // stops streaming
+	}
+	n, ok = s.volume.Stream(samples)
+	return n, ok
+}
+
+func (s *PlayerStreamer) Err() error {
+	return s.streamer.Err()
+}
