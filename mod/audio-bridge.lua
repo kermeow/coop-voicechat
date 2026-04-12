c43d416e2b2378afb4f66198a2a92bf493d19114
jayden <46307433+kermeow@users.noreply.github.com>
2026-04-12 12:33:29 +0100

    update mod to new bridge

--- mod/audio-bridge.lua
+++ mod/audio-bridge.lua
@@ -1,137 +1,125 @@
-local MAGIC_NUMBER = "smvc"
-local PACKET_HEADER_FMT = "!1<BI4B"
+local FILE_HEADER = "smvc"
 
-do
-    gVoiceStates = {}
-    for i = 0, MAX_PLAYERS - 1 do
-        gVoiceStates[i] = {
-            volume = 1,
-            speakVol = -1,
-            frames = {}
-        }
-    end
+local PACK_PREFIX = "!1<"
+local PACKET_MAX_SIZE = 1400 -- try to keep under mtu to avoid splitting, not the actual max (3000)
+
+local MAX_AUDIO_FRAMES = 100
+
+gVoiceStates = {}
+for i = 0, MAX_PLAYERS - 1 do
+    gVoiceStates[i] = {
+        audioFile = "stream_" .. tostring(i),
+        audioFrames = {},
+
+        volume = 1,
+        speakVol = -1
+    }
 end
 
----@param i number
----@param file ModFsFile
-local function write_state_to_file(i, file)
-    local voiceState = gVoiceStates[i]
-    local marioState = gMarioStates[i]
-    local networkPlayer = gNetworkPlayers[i]
+local function send_audio_packet(packet)
+    local globalIndex = network_global_index_from_local(0)
+    local prefix = string.pack(PACK_PREFIX .. "B", globalIndex)
+    network_send_bytestring(false, prefix .. packet)
+end
 
-    file:write_number(voiceState.volume, FLOAT_TYPE_F32)
+local function recv_audio_packet(packet)
+    local globalIndex = string.unpack(PACK_PREFIX .. "B", packet)
+    local localIndex = network_local_index_from_global(globalIndex)
 
-    local headHeight = marioState.marioObj.hitboxHeight - 60
+    local lVoiceState = gVoiceStates[localIndex]
 
-    file:write_number(marioState.pos.x, FLOAT_TYPE_F64)
-    file:write_number(marioState.pos.y + headHeight, FLOAT_TYPE_F64)
-    file:write_number(marioState.pos.z, FLOAT_TYPE_F64)
+    packet = string.sub(packet, 2)
 
-    file:write_integer(networkPlayer.currLevelNum, INT_TYPE_U8)
-    file:write_integer(networkPlayer.currAreaIndex, INT_TYPE_U8)
+    while #packet > 0 do
+        local timestamp, len = string.unpack(PACK_PREFIX .. "I4I4", packet)
+        packet = string.sub(packet, 9)
 
-    if i == 0 then
-        file:write_number(gLakituState.curPos.x, FLOAT_TYPE_F64)
-        file:write_number(gLakituState.curPos.y, FLOAT_TYPE_F64)
-        file:write_number(gLakituState.curPos.z, FLOAT_TYPE_F64)
+        local data = string.sub(packet, 1, len)
+        table.insert(lVoiceState, { syncFrame = 0, timestamp = timestamp, data = data })
 
-        file:write_number(gLakituState.curFocus.x, FLOAT_TYPE_F64)
-        file:write_number(gLakituState.curFocus.y, FLOAT_TYPE_F64)
-        file:write_number(gLakituState.curFocus.z, FLOAT_TYPE_F64)
+        packet = string.sub(packet, len + 1)
     end
 end
 
 function audio_recv()
-    local recording = gVoiceBridge.recvFS:get_file("recording")
-    if not recording then
+    local stream = gVoiceBridge.recvFs:get_file("stream")
+    if not stream then
         return
     end
-    recording:seek(16, FILE_SEEK_SET)
 
-    local frames = 0
-    local order = 0
-    while not recording:is_eof() do
-        local syncFrame = recording:read_integer(INT_TYPE_U32)
-        local len = recording:read_integer(INT_TYPE_U32)
-        local data = recording:read_bytes(len)
-        if syncFrame > gVoiceBridge.syncLastRemoteFrame then
-            local raw = MAGIC_NUMBER ..
-                string.pack(PACKET_HEADER_FMT, network_global_index_from_local(0), gVoiceBridge.syncLocalFrame, order) ..
-                data
-            network_send_bytestring(false, raw)
-            -- on_bytestring_receive(raw)
-            frames = frames + 1
-            order = order + 1
+    stream:seek(4, FILE_SEEK_SET) -- skip file header
+
+    local packet = ""
+
+    ::read::
+    while not stream:is_eof() do
+        local syncFrame = stream:read_integer(INT_TYPE_U32)
+        local timestamp = stream:read_integer(INT_TYPE_U32)
+        local len = stream:read_integer(INT_TYPE_U32)
+        local data = stream:read_bytes(len)
+        if syncFrame < gVoiceBridge.syncRemoteFrame then
+            goto read
         end
-    end
-
-    local volumes = gVoiceBridge.recvFS:get_file("volumes")
-    if not volumes then
-        return
-    end
-    volumes:seek(17, FILE_SEEK_SET)
-
-    local newVolumes = {}
-    while not volumes:is_eof() do
-        local i = volumes:read_integer(INT_TYPE_U8)
-        local rms = volumes:read_number(FLOAT_TYPE_F32)
-        newVolumes[i] = rms
-    end
-    for i, voiceState in pairs(gVoiceStates) do
-        local volume = newVolumes[i]
-        if not volume then
-            volume = -1
-        else
-            volume = math.min(1, math.sqrt(volume) * 2)
+        local segment = string.pack(PACK_PREFIX .. "I4I4", timestamp, len) .. data
+        if #segment > PACKET_MAX_SIZE then
+            -- frame is way too big, send separately
+            send_audio_packet(segment)
+            goto read
         end
-        voiceState.speakVol = volume
+        if #packet + #segment > PACKET_MAX_SIZE then
+            send_audio_packet(packet)
+            packet = segment
+            goto read
+        end
+        packet = packet .. segment
+    end
+
+    if #packet > 0 then
+        send_audio_packet(packet)
     end
 end
 
 function audio_send()
-    local localFile = mod_fs_get_or_create_file(gVoiceBridge.sendFS, "local", false)
-    mod_fs_file_clear(localFile)
-    mod_fs_file_set_compression(localFile, 0)
-    localFile:write_bytes("smvc local data ")
-    write_state_to_file(0, localFile)
+    local i = 0
 
-    local states = mod_fs_get_or_create_file(gVoiceBridge.sendFS, "states", false)
-    mod_fs_file_clear(states)
-    mod_fs_file_set_compression(states, 0)
-    states:write_bytes("smvc state data ")
+    ::write::
+    while i < MAX_PLAYERS - 1 do
+        i = i + 1
 
-    for i = 1, MAX_PLAYERS - 1 do
-        local voiceState = gVoiceStates[i]
-        local fileName = string.format("voice-%d", i)
-        if gNetworkPlayers[i].connected then
-            states:write_integer(i, INT_TYPE_U8)
-            states:write_integer(#fileName, INT_TYPE_U8)
-            states:write_bytes(fileName)
-            write_state_to_file(i, states)
+        local lVoiceState = gVoiceStates[i]
+        local lNetworkPlayer = gNetworkPlayers[i]
 
-            local sendFile = mod_fs_get_or_create_file(gVoiceBridge.sendFS, fileName, false)
-            mod_fs_file_clear(sendFile)
-            mod_fs_file_set_compression(sendFile, 0)
-            sendFile:write_bytes("smvc output data ")
-
-            -- todo: sort the frames just in case :p
-            for i = #voiceState.frames, 1, -1 do
-                local frame = voiceState.frames[i]
-                if frame.syncFrame > 0 and frame.syncFrame <= gVoiceBridge.syncRemoteAckFrame then
-                    table.remove(voiceState.frames, i)
-                end
+        if not lNetworkPlayer.connected then
+            if gVoiceBridge.sendFs:get_file(lVoiceState.audioFile) then
+                gVoiceBridge.sendFs:delete_file(lVoiceState.audioFile)
             end
+            goto write
+        end
 
-            for _, frame in pairs(voiceState.frames) do
-                if frame.syncFrame == 0 then
-                    frame.syncFrame = gVoiceBridge.syncLocalFrame
-                end
-                sendFile:write_integer(frame.syncFrame, INT_TYPE_U32)
-                sendFile:write_integer(string.len(frame.data), INT_TYPE_U32)
-                sendFile:write_bytes(frame.data)
+        local stream = mod_fs_get_or_create_file(gVoiceBridge.sendFs, lVoiceState.audioFile, false)
+        mod_fs_file_clear(stream)
+        stream:write_bytes(FILE_HEADER)
+
+        local deadFrames = math.max(0, #lVoiceState.audioFrames - MAX_AUDIO_FRAMES)
+
+        for i, frame in pairs(lVoiceState.audioFrames) do
+            if frame.syncFrame == 0 then
+                frame.syncFrame = gVoiceBridge.syncLocalFrame
             end
-        else
-            states:write_integer(i | 0x80, INT_TYPE_U8)
+            if frame.syncFrame < gVoiceBridge.syncRemoteAckFrame then
+                deadFrames = math.max(deadFrames, i)
+            else
+                stream:write_integer(frame.syncFrame, INT_TYPE_U32)
+                stream:write_integer(frame.timestamp, INT_TYPE_U32)
+                stream:write_integer(#frame.data, INT_TYPE_U32)
+                stream:write_bytes(frame.data)
+            end
+        end
+
+        if deadFrames > 0 then
+            for _ = 1, deadFrames do
+                table.remove(lVoiceState.audioFrames, 1)
+            end
         end
     end
 end
@@ -141,26 +129,7 @@ local function on_bytestring_receive(raw)
     if not gVoiceBridge.connected then
         return
     end
-
-    local packet = string.match(raw, "^" .. MAGIC_NUMBER .. "(.*)")
-    if packet then
-        local globalIndex, frame, order = string.unpack(PACKET_HEADER_FMT, packet)
-        local data = string.sub(packet, 7)
-        local localIndex = network_local_index_from_global(globalIndex)
-
-        local voiceState = gVoiceStates[localIndex]
-
-        while #voiceState.frames >= 64 do
-            table.remove(voiceState.frames, 1)
-        end
-
-        table.insert(voiceState.frames, {
-            syncFrame = 0,
-            frame = frame,
-            order = order,
-            data = data
-        })
-    end
+    recv_audio_packet(raw)
 end
 
 hook_event(HOOK_ON_PACKET_BYTESTRING_RECEIVE, on_bytestring_receive)
