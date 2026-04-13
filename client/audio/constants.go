47c237dc6dd9fb29ecde698191a26ee571578488
jayden <46307433+kermeow@users.noreply.github.com>
2026-04-13 23:10:57 +0100

    static-rnnoise

--- client/audio/constants.go
+++ client/audio/constants.go
@@ -1,6 +1,9 @@
 package audio
 
+import "github.com/gopxl/beep/v2"
+
 const SAMPLE_RATE = 48_000 // 48khz
+var SAMPLE_RATE_BEEP = beep.SampleRate(SAMPLE_RATE)
 
 const OPUS_FRAME_SIZE = 0.02 // 20 ms
 const OPUS_FRAME_SAMPLES = SAMPLE_RATE * OPUS_FRAME_SIZE
