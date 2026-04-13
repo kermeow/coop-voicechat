ff34205c143659cc50bd24bdf76d97744a9ee1f4
jayden <46307433+kermeow@users.noreply.github.com>
2026-04-13 22:21:59 +0100

    implement rnnoise

--- client/audio/opus_streamer.go
+++ client/audio/opus_streamer.go
@@ -17,53 +17,53 @@ func NewOpusStreamer() *OpusStreamer {
 	return s
 }
 
-func (s *OpusStreamer) Push(packet []byte, timestamp int) {
-	s.jitter.Push(&JitterPacket{
+func (o *OpusStreamer) Push(packet []byte, timestamp int) {
+	o.jitter.Push(&JitterPacket{
 		Data:      packet,
 		Timestamp: timestamp,
 	})
 }
 
-func (s *OpusStreamer) Stream(samples [][2]float64) (n int, ok bool) {
+func (o *OpusStreamer) Stream(samples [][2]float64) (n int, ok bool) {
 	nSamples := len(samples)
 
-	opusSamples := make([]float32, len(samples))
-	filled := copy(opusSamples, s.leftover)
-	s.leftover = s.leftover[filled:]
+	filled := o.convert(o.leftover, samples)
+	o.leftover = o.leftover[filled:]
 
 	for filled < nSamples {
-		packet, _ := s.jitter.Pop()
+		packet, _ := o.jitter.Pop()
 
 		var dec []float32
 		if packet != nil {
-			dec, s.err = s.decoder.Decode(packet.Data)
-			if s.err != nil {
+			dec, o.err = o.decoder.Decode(packet.Data)
+			if o.err != nil {
 				return filled, false
 			}
 		} else {
-			dec, s.err = s.decoder.DecodePLC(OPUS_FRAME_SAMPLES)
-			if s.err != nil {
+			dec, o.err = o.decoder.DecodePLC(OPUS_FRAME_SAMPLES)
+			if o.err != nil {
 				return filled, false
 			}
 		}
 
-		copied := copy(opusSamples[filled:], dec)
-		if copied < len(dec) {
-			s.leftover = make([]float32, len(dec)-copied)
-			copy(s.leftover, dec[copied:])
-		}
+		copied := o.convert(dec, samples[filled:])
+		o.leftover = dec[copied:]
 		filled += copied
-	}
-
-	for i, sample := range opusSamples {
-		f64 := math.Tanh(float64(sample)) // soft clip
-		samples[i][0] = f64
-		samples[i][1] = f64
 	}
 
 	return filled, true
 }
 
-func (s *OpusStreamer) Err() error {
-	return s.err
+func (o *OpusStreamer) Err() error {
+	return o.err
+}
+
+func (o *OpusStreamer) convert(in []float32, out [][2]float64) int {
+	fill := min(len(in), len(out))
+	for i := range fill {
+		f64 := math.Tanh(float64(in[i])) // soft clip
+		out[i][0] = f64
+		out[i][1] = f64
+	}
+	return fill
 }
--- client/bridge/audio_bridge.go
+++ client/bridge/audio_bridge.go
@@ -3,12 +3,11 @@ package bridge
 import (
 	"context"
 	"coop-voicechat/audio"
+	"coop-voicechat/audio/effects"
 	"coop-voicechat/coop"
 	"log"
-	"math"
 
 	"github.com/gopxl/beep/v2/speaker"
-	"github.com/gordonklaus/portaudio"
 )
 
 const MAX_INPUT_FRAMES = 100
@@ -23,12 +22,15 @@ type AudioBridge struct {
 	bridge    *Bridge
 	streamers map[uint8]*audio.PlayerStreamer
 
-	paInStream  *portaudio.Stream
-	paInBuffer  []float32
-	inRms       float64
-	inTimestamp int
-	inQueue     []*frame
-	opusEncoder *audio.OpusEncoder
+	inputStreamer *audio.InputStreamer
+	denoiser      *effects.Denoiser
+	analyzer      *effects.Analyzer
+	// paInStream  *portaudio.Stream
+	// paInBuffer  []float32
+	// inRms       float64
+	// inTimestamp int
+	// inQueue     []*frame
+	// opusEncoder *audio.OpusEncoder
 }
 
 func NewAudioBridge(b *Bridge) *AudioBridge {
@@ -36,56 +38,63 @@ func NewAudioBridge(b *Bridge) *AudioBridge {
 		bridge:    b,
 		streamers: make(map[uint8]*audio.PlayerStreamer),
 
-		paInBuffer:  make([]float32, audio.OPUS_FRAME_SAMPLES),
-		inTimestamp: 0,
-		inQueue:     make([]*frame, 0),
-		opusEncoder: audio.NewOpusEncoder(),
+		// paInBuffer:  make([]float32, audio.OPUS_FRAME_SAMPLES),
+		// inTimestamp: 0,
+		// inQueue:     make([]*frame, 0),
+		// opusEncoder: audio.NewOpusEncoder(),
 	}
-	paInStream, _ := portaudio.OpenDefaultStream(1, 0, audio.SAMPLE_RATE, audio.OPUS_FRAME_SAMPLES, a.paInBuffer)
-	a.paInStream = paInStream
+	// paInStream, _ := portaudio.OpenDefaultStream(1, 0, audio.SAMPLE_RATE, audio.OPUS_FRAME_SAMPLES, a.paInBuffer)
+	// a.paInStream = paInStream
+	a.inputStreamer = &audio.InputStreamer{}
+	a.inputStreamer.StartDefault()
+
+	a.denoiser = effects.NewDenoiser(a.inputStreamer)
+	a.analyzer = &effects.Analyzer{Streamer: a.denoiser}
+
+	speaker.Play(a.analyzer)
 	return a
 }
 
 func (a *AudioBridge) encodeNext() {
-	err := a.paInStream.Read()
-	if err != nil {
-		log.Println("Error reading input:", err)
-		return
-	}
+	// err := a.paInStream.Read()
+	// if err != nil {
+	// 	log.Println("Error reading input:", err)
+	// 	return
+	// }
 
-	if !a.bridge.Connected {
-		return
-	}
+	// if !a.bridge.Connected {
+	// 	return
+	// }
 
-	rms := 0.0
-	for i := range a.paInBuffer {
-		vol := float64(a.paInBuffer[i])
-		rms += vol * vol
-		a.paInBuffer[i] = float32(math.Tanh(vol * math.Pow(coop.LocalPlayer.State.Volume, 2)))
-	}
-	rms = math.Sqrt(rms / float64(len(a.paInBuffer)))
-	a.inRms = rms
+	// rms := 0.0
+	// for i := range a.paInBuffer {
+	// 	vol := float64(a.paInBuffer[i])
+	// 	rms += vol * vol
+	// 	a.paInBuffer[i] = float32(math.Tanh(vol * math.Pow(coop.LocalPlayer.State.Volume, 2)))
+	// }
+	// rms = math.Sqrt(rms / float64(len(a.paInBuffer)))
+	// a.inRms = rms
 
-	timestamp := a.inTimestamp
-	a.inTimestamp++
+	// timestamp := a.inTimestamp
+	// a.inTimestamp++
 
-	data, err := a.opusEncoder.Encode(a.paInBuffer)
-	if err != nil {
-		log.Println("Error encoding input:", err)
-		return
-	}
+	// data, err := a.opusEncoder.Encode(a.paInBuffer)
+	// if err != nil {
+	// 	log.Println("Error encoding input:", err)
+	// 	return
+	// }
 
-	f := &frame{
-		syncFrame: 0,
-		timestamp: timestamp,
-		data:      data,
-	}
-	i := max(0, len(a.inQueue)-(MAX_INPUT_FRAMES-1))
-	a.inQueue = append(a.inQueue[i:], f)
+	// f := &frame{
+	// 	syncFrame: 0,
+	// 	timestamp: timestamp,
+	// 	data:      data,
+	// }
+	// i := max(0, len(a.inQueue)-(MAX_INPUT_FRAMES-1))
+	// a.inQueue = append(a.inQueue[i:], f)
 }
 
 func (a *AudioBridge) connect() {
-	a.inQueue = a.inQueue[:0]
+	// a.inQueue = a.inQueue[:0]
 }
 
 func (a *AudioBridge) disconnect() {
@@ -117,7 +126,7 @@ func (a *AudioBridge) removePlayer(localIndex uint8) {
 func (a *AudioBridge) run(ctx context.Context) {
 	log.Println("Audio bridge running")
 
-	a.paInStream.Start()
+	// a.paInStream.Start()
 
 running:
 	for {
@@ -125,8 +134,8 @@ running:
 		case <-ctx.Done():
 			a.stop()
 			break running
-		default:
-			a.encodeNext()
+			// default:
+			// 	a.encodeNext()
 		}
 	}
 }
@@ -134,7 +143,7 @@ running:
 func (a *AudioBridge) stop() {
 	log.Println("Audio bridge stopping")
 
-	a.paInStream.Abort()
+	// a.paInStream.Abort()
 
 	for i := range a.streamers {
 		a.removePlayer(i)
@@ -166,24 +175,24 @@ func (a *AudioBridge) send() {
 	in := a.bridge.SendFs.Create("stream")
 	in.WriteBytes(FILE_HEADER_BYTES)
 
-	for _, f := range a.inQueue {
-		if f.syncFrame == 0 {
-			f.syncFrame = a.bridge.syncLocalFrame
-		}
-		if f.syncFrame < a.bridge.syncRemoteAckFrame-1 {
-			continue
-		}
-		in.WriteUint32(f.syncFrame)
-		in.WriteUint32(uint32(f.timestamp))
-		in.WriteUint32(uint32(len(f.data)))
-		in.WriteBytes(f.data)
-	}
+	// for _, f := range a.inQueue {
+	// 	if f.syncFrame == 0 {
+	// 		f.syncFrame = a.bridge.syncLocalFrame
+	// 	}
+	// 	if f.syncFrame < a.bridge.syncRemoteAckFrame-1 {
+	// 		continue
+	// 	}
+	// 	in.WriteUint32(f.syncFrame)
+	// 	in.WriteUint32(uint32(f.timestamp))
+	// 	in.WriteUint32(uint32(len(f.data)))
+	// 	in.WriteBytes(f.data)
+	// }
 
 	vols := a.bridge.SendFs.Create("loudness")
 	vols.WriteBytes(FILE_HEADER_BYTES)
 
 	vols.WriteUint8(0)
-	vols.WriteFloat64(a.inRms)
+	// vols.WriteFloat64(a.inRms)
 
 	for i, s := range a.streamers {
 		vols.WriteUint8(i)
